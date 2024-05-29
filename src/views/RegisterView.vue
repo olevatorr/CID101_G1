263d873ad2fd90e1127b6056af539ce1c47403b1
Rahaydn <lexea53@gmail.com>
2024-05-29 23:00:21 +0800

    會員註冊完成

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,33 +3,47 @@
             <div class="container">
                 <div class="register-box col-md-12">
                     <div class="register-txt">
-                        <div class="register-login">
-                            <label>會員姓名</label>
-                            <input type="text" placeholder="請輸入姓名">
-                            <label>會員帳號</label>
-                            <input type="text" name="" id="" placeholder="請輸入帳號">
-                            <label>會員密碼</label>
-                            <input type="number" name="" id="" placeholder="請輸入密碼">
-                            <label>確認密碼</label>
-                            <input type="number" name="" id="" placeholder="再輸入一次密碼">
-                            <label>電話號碼</label>
-                            <input type="tel" name="" id="" placeholder="請輸入電話">
-                            <label>信箱</label>
-                            <input type="email" name="" id="" placeholder="請輸入信箱">
-                            <label>地址</label>
-                            <input type="text" name="" id="" placeholder="請輸入地址">
+                        <div class="register-login col-md-6">
+                            <div class="from-group">
+                                <label>會員姓名</label>
+                                <input type="text" placeholder="請輸入姓名">
+                            </div>
+                            <div class="from-group">
+                                <label>會員帳號</label>
+                                <input type="text" name="" id="" placeholder="請輸入帳號">
+                            </div>
+                            <div class="from-group">
+                                <label>會員密碼</label>
+                                <input type="number" name="" id="" placeholder="請輸入密碼">
+                            </div>
+                            <div class="from-group">
+                                <label>確認密碼</label>
+                                <input type="number" name="" id="" placeholder="再輸入一次密碼">
+                            </div>
+                            <div class="from-group">
+                                <label>電話號碼</label>
+                                <input type="tel" name="" id="" placeholder="請輸入電話">
+                            </div>
+                            <div class="from-group writing">
+                                <label>信箱</label>
+                                <input type="email" name="" id="" placeholder="請輸入信箱">
+                            </div>
+                            <div class="from-group writing">
+                                <label>地址</label>
+                                <input type="text" name="" id="" placeholder="請輸入地址">
+                            </div>
                             <button>立即註冊</button>
+                            <div class="other-login">
+                                <p>其他登入方式</p>
+                                <a href="#"><i class="fa-brands fa-google-plus"></i></a>
+                                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                <a href="#"><i class="fa-brands fa-line"></i></a>
+                            </div>
                         </div>
-                        <div class="other-login">
-                            <p>其他登入方式</p>
-                            <a href="#"><i class="fa-brands fa-google-plus"></i></a>
-                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                            <a href="#"><i class="fa-brands fa-line"></i></a>
-                        </div>
-                        <div class="register-box-img">
+                        <div class="register-box-img col-md-6">
                             <img src="../../public/img/member/member7.png" alt="會員背景圖片">
-                            <div class="member-logo-box">
-                                <img src="../../public/img/member/member3.png" alt="logo">
+                            <div class="register-logo-box">
+                                <img src="../../public/img/LOGO-white.png" alt="logo">
                                 <span>潔淨海洋&emsp;綠色明天</span>
                             </div>
                         </div>
