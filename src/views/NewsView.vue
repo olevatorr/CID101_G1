f47c2a099b5ebc354ee366869657b98777b4f67a
Rahaydn <lexea53@gmail.com>
2024-05-31 23:13:11 +0800

    有問題

---
+++ src/views/NewsView.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="section section-news-banner">
+        <div class="container">
+            <div class="news-banner-txt">
+                <div class="news-banner_title">
+                    <p>最新消息</p>
+                    <h1>NEWS</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section section-news-content">
+        <div class="container">
+            <h4 class="">最新消息</h4>
+            <div class="news-filter">
+                <button>全部</button>
+                <button>最新商品</button>
+                <button>企業捐款</button>
+                <button>宣導</button>
+            </div>
+            <div class="row">
+                <div class="col-6 col-md-4 col-lg-3">
+                    <RouterLink to="/Newsinner">
+                    <!-- 放入app -->
+                        <div class="news-card">
+                            <div class="news-pic">
+                                <img src="https://picsum.photos/300/200/?random=10">
+                            </div>
+                            <div class="txt">
+                                <p>Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Itaque nostrum impedit
+                                dolore praesentium repellendus quidem debitis quas magni sed fuga?</p>
+                            </div>
+                        </div>
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
