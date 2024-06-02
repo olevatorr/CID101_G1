df3f04efa4f6ef7f2440b2c160e742bf6586d65c
Rahaydn <lexea53@gmail.com>
2024-06-02 20:19:58 +0800

    組長檢查

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -1,36 +1,38 @@
 <template>
-        <section class="section section-member body-member">
-            <div class="container">
-                <div class="member-box col-md-12">
-                    <div class="member-box-img col-sm-6 col-md-6">
-                        <img src="../../public/img/member/member2.jpg" alt="會員背景圖片">
+    <section class="section section-member">
+        <div class="container">
+                <div class="member-box row">
+                    <div class="member-box-img col-12 col-md-6">
                         <div class="member-logo-box">
-                            <img src="../../public/img/LOGO-white.png" alt="logo">
+                            <div class="img">
+                                <img src="../../public/img/member/logo.png" alt="logo">
+                            </div>
                             <span>潔淨海洋&emsp;綠色明天</span>
+                            <RouterLink to="/RegisterView"><button>加入會員</button></RouterLink>
                         </div>
                     </div>
-                    <div class="member-txt col-sm-6 col-md-6">
+                    <div class="member-txt col-12 col-md-6">
                         <div class="member-login">
                             <label>帳號</label>
                             <input type="text" maxlength="10" placeholder="請輸入帳號">
                             <label>密碼</label>
                             <input type="password" name="" id="" maxlength="10" placeholder="請輸入密碼">
                         </div>
-                        <div class="member-button">
-                            <RouterLink to="/ProfileView"><button>&emsp;&emsp;登入&emsp;&emsp;</button></RouterLink>
-                            <button class="member-btn">
-                                <RouterLink to="/RegisterView">加入會員</RouterLink>
-                            </button>
-                        </div>
                         <span class="forgot">
                             <RouterLink to="/ForgetPasswordView">忘記密碼?</RouterLink>
+                            <div class="member-button">
+                                <RouterLink to="/RegisterView"><button class="add">加入會員</button></RouterLink>
+                                <RouterLink to="/ProfileView"><button>&emsp;&emsp;登入&emsp;&emsp;</button></RouterLink>
+                            </div>
                         </span>
                         <p>其他登入方式</p>
-                        <a href="#"><i class="fa-brands fa-google"></i></a>
-                        <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                        <a href="#"><i class="fa-brands fa-line"></i></a>
+                        <div class="third-party">
+                            <a href="#"><i class="fa-brands fa-google"></i></a>
+                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#"><i class="fa-brands fa-line"></i></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+        </div>
+    </section>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="section section-register">
         <div class="container">
-            <div class="register-box">
-                <div class="register-txt col-md-5">
+            <div class="register-box row">
+                <div class="register-txt col-12 col-md-6">
                     <div class="register-login ">
                         <div class="from-group">
                             <label>會員姓名</label>
@@ -24,31 +24,34 @@
                             <label>電話號碼</label>
                             <input type="tel" name="" id="" placeholder="請輸入電話">
                         </div>
-                        <div class="from-group writing">
-                            <label>信箱</label>
+                        <div class="from-group ">
+                            <label>&emsp;信箱&emsp;</label>
                             <input type="email" name="" id="" placeholder="請輸入信箱">
                         </div>
-                        <div class="from-group writing">
-                            <label>地址</label>
+                        <div class="from-group ">
+                            <label>&emsp;地址&emsp;</label>
                             <input type="text" name="" id="" placeholder="請輸入地址">
                         </div>
                         <button>立即註冊</button>
                         <div class="other-login">
                             <p>其他登入方式</p>
+                            <div class="third-party">
                             <a href="#"><i class="fa-brands fa-google"></i></a>
                             <a href="#"><i class="fa-brands fa-facebook"></i></a>
                             <a href="#"><i class="fa-brands fa-line"></i></a>
                         </div>
+                        </div>
                     </div>
                 </div>
-                <div class="register-box-img col-md-5">
-                    <img src="../../public/img/member/member7.png" alt="會員背景圖片">
+                <div class="register-box-img col-12 col-md-6">
                     <div class="register-logo-box">
-                        <img src="../../public/img/LOGO-white.png" alt="logo">
+                        <div class="img">
+                            <img src="../../public/img/member/logo.png" alt="logo">
+                        </div>
                         <span>潔淨海洋&emsp;綠色明天</span>
+                        <RouterLink to="/RegisterView"><button class="add">加入會員</button></RouterLink>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
