04c45bca63cb63bcfb39d107c3064a030ee7e0fc
olevatorr <otis.coding@gmail.com>
2024-06-16 00:42:58 +0800

    修正news組件/news篩選/home news組件/home event改fetch

--- src/components/Home/HomeFeedback.vue
+++ src/components/Home/HomeFeedback.vue
@@ -1,5 +1,14 @@
 <script setup>
-
+import shareCard from '@/components/ShareCard.vue';
+import { onMounted, ref } from 'vue';
+const homeFeedback = ref([])
+onMounted(()=>{
+    fetch('../../../public/Share.json')
+        .then(res => res.json())
+        .then(jsonData=>{
+            homeFeedback.value = jsonData.slice(0,12)
+        })
+})
 </script>
 <template>
     <section class="section section-index-event-feedback">
@@ -8,6 +17,9 @@
                 FEEDBACK OF EVENT<br>
                 活動分享
             </h3>
+            <div class="row">
+                <shareCard />
+            </div>
         </div>
     </section>
 </template>
