00be186c4a640d8ae9689f8d96a3f1d5f12ed259
justin <justin20745413@gmail.com>
2024-06-16 01:59:41 +0800

    add捐款功能

---
+++ src/components/Donate/DonateCart.vue
@@ -0,0 +1,66 @@
+<template>
+    <section class="section section-mallcart-cart">
+        <div class="container">
+            <div class="cart-wrap">
+                <div class="title">
+                    <h3>01</h3>
+                    <h3>購物車內容</h3>
+                </div>
+                <div class="cart-head">
+                    <p>項目</p>
+                    <p>商品資料</p>
+                    <p>小計</p>
+                </div>
+            </div>
+            <div class="cart-info">
+                <div class="item">
+                    <p>1</p>
+                </div>
+                <div class="content">
+                    <div class="pic">
+                        <img src="../../../public/img/donate/Mask group.png" alt="">
+                    </div>
+                    <div class="pic">
+                        <p>【為海洋生態出一份力】</p>
+                    </div>
+                </div>
+                <div class="price">
+                    <!-- 顯示捐款金額 -->
+                    <p>NT$ {{ donationAmount }}</p>
+                </div>
+                <div class="subtotal">
+                    <i class="fa-regular fa-circle-xmark"></i>
+                </div>
+            </div>
+            <div class="cart-price">
+                <div class="text">
+                    <h3>訂單金額 NT$ {{ donationAmount }}</h3>
+                </div>
+            </div>
+            <div class="cart-total">
+                <div class="text">
+                    <h3>應付金額 NT$ {{ donationAmount }}</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+// 引入必要的模塊
+import Cookies from 'js-cookie';
+
+export default {
+    name: 'DonateCart', // 組件名稱
+    data() {
+        return {
+            donationAmount: 0, // 存儲捐款金額
+        };
+    },
+    mounted() {
+        // 從 Cookie 中讀取捐款金額
+        this.donationAmount = Cookies.get('donationAmount');
+    },
+};
+</script>
+
---
+++ src/components/Donate/OceanStory.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="section section-story">
+        <div class="container container-story ">
+            <div class="flex">
+                <div class="row">
+                    <div class="row pic-1 col-6 col-md-3">
+                        <img src="/public/img/donate/donate-pic2.png" />
+                        <div class="pic2">
+                            <img src="/public/img/donate/donate-pic3.png" />
+                        </div>
+                    </div>
+
+                    <div class="pic3 col-6 col-md-3">
+                        <img src="/public/img/donate/donate-pic4.png" />
+                    </div>
+
+                    <div class="story-text col-12 col-sm-6">
+                        <h2>海洋故事</h2>
+                        <p>
+                            滔滔大海,賦予地球生命的搖籃，孕育著萬物的希望。<br />
+                            如今的海洋正遭受著人類活動的嚴重威脅：塑料污染、過度捕撈、氣候變遷等，正在無情地摧毀著這片寶貴的藍色家園。<br />
+                            但是，我們還有機會扭轉乾坤！您的每一份捐助，都將化作守護海洋生態的力量，為珊瑚礁注入新生，為海豚們帶來自由的暢遊，為鯨魚們開闢安全的回家路。<br />
+                            讓我們攜手同心，共同捍衛這片深邃而神奇的海洋世界。每一分捐款，都將如滴水匯入大海,彙聚成守護之流，灌溉海洋生命的根基。<br />
+                            加入我們，成為海洋的守護者！讓這片孕育萬物的母親之海，永遠綻放勃勃生機!讓我們用實際行動，捍衛美麗家園的未來!
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
