45a44bc433f865cfd2832ea4cdcc5002449a8f37
Rahaydn <lexea53@gmail.com>
2024-06-19 20:23:54 +0800

    訂單檢視按鈕功能完成，RWD

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -23,11 +23,8 @@
             <RouterLink to="/Member"><button class="btn" @click="logout">會員登出</button></RouterLink>
           </div>
         </div>
-        <select name="pfl-pets" id="" class="profile-pets"
-        v-model="selectedOption"
-        @change="selectOption"
-        >
-          <option value="profile" >會員資料修改</option>
+        <select name="pfl-pets" id="" class="profile-pets" v-model="selectedOption" @change="selectOption">
+          <option value="profile">會員資料修改</option>
           <option value="password">密碼修改</option>
           <option value="activity">活動查詢</option>
           <option value="orders">訂單查詢</option>
@@ -39,23 +36,23 @@
           <ul>
             <li>
               <label for="">會員姓名</label>
-              <input type="text" name="" id="" maxlength="20" v-model="member.U_NAME" >
+              <input type="text" name="" id="" maxlength="20" v-model="member.U_NAME">
             </li>
             <li>
               <label for="">會員帳號</label>
-              <input type="text" name="" id="" maxlength="20" v-model="member.U_ACCOUNT" >
+              <input type="text" name="" id="" maxlength="20" v-model="member.U_ACCOUNT">
             </li>
             <li>
               <label for="">會員信箱</label>
-              <input type="text" name="" id="" v-model="member.U_EMAIL" >
+              <input type="text" name="" id="" v-model="member.U_EMAIL">
             </li>
             <li>
               <label for="">會員電話</label>
-              <input type="tel" name="" id="" maxlength="10" v-model="member.U_PHONE" >
+              <input type="tel" name="" id="" maxlength="10" v-model="member.U_PHONE">
             </li>
             <li>
               <label for="">會員地址</label>
-              <input type="text" name="" id="" v-model="member.U_ADDRESS" >
+              <input type="text" name="" id="" v-model="member.U_ADDRESS">
             </li>
           </ul>
           <div class="store"><button>儲存變更</button><button>取消變更</button></div>
@@ -66,132 +63,141 @@
           <table class="order">
             <caption>訂單查詢</caption>
             <thead>
-            <tr>
-              <th>訂單編號</th>
-              <th>訂單狀況</th>
-              <th>訂單日期</th>
-              <th>總金額</th>
-              <th>付款方式</th>
-              <th colspan="2">功能</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="list in paginatedData" :key="list.PO_ID">
-              <td data-label="訂單編號">{{list.PO_ID}}</td>
-              <td data-label="訂單狀況">{{list.S_STATUS}}</td>
-              <td data-label="訂單日期">{{list.PO_DATE}}</td>
-              <td data-label="總金額">{{list.PO_AMOUNT}} </td>
-              <td data-label="付款方式">{{list.PO_ORDER}}</td>
-              <td><button class="view" @click="toggleShopTable(list.PO_ID)">{{list.VIEW}}</button></td>
-              <td data-label="功能"><button>{{list.ORDER_BUTTON}}</button></td>
-            </tr>
-          </tbody>
+              <tr>
+                <th>訂單編號</th>
+                <th>訂單狀況</th>
+                <th>訂單日期</th>
+                <th>總金額</th>
+                <th>付款方式</th>
+                <th colspan="2">功能</th>
+              </tr>
+            </thead>
+            <tbody>
+              <template v-for="list in paginatedData" :key="list.PO_ID">
+                <tr>
+                  <td data-label="訂單編號">{{ list.PO_ID }}</td>
+                  <td data-label="訂單狀況">{{ list.S_STATUS }}</td>
+                  <td data-label="訂單日期">{{ list.PO_DATE }}</td>
+                  <td data-label="總金額">{{ list.PO_AMOUNT }} </td>
+                  <td data-label="付款方式">{{ list.PO_ORDER }}</td>
+                  <td><button class="view" @click="toggleShopTable(list.PO_ID)">{{ list.VIEW }}</button></td>
+                  <td data-label="功能"><button>{{ list.ORDER_BUTTON }}</button></td>
+                </tr>
+                <!-- 商品資訊最後要放進div做成toggle -->
+                <!--currentPoId的值等於list.PO_ID 和 isShopTableVisible開啟的狀態 -->
+                <div v-if="currentPoId === `${list.PO_ID}` && isShopTableVisible"
+                  class="shoptable">
+                  <table class="shop">
+                    <caption>訂單明細</caption>
+                    <tr>
+                      <th>商品編號</th>
+                      <th>商品名稱</th>
+                      <th>商品數量</th>
+                      <th>單價</th>
+                    </tr>
+                    <tr v-for="shops in filteredShoplists" :key="shops.PO_ID">
+                      <td data-label="商品編號">{{ shops.P_ID }}</td>
+                      <td data-label="商品名稱">{{ shops.P_NAME }}</td>
+                      <td data-label="商品數量">{{ shops.PO_QTY }}</td>
+                      <td data-label="單價">{{ shops.PO_PRICE }}</td>
+                    </tr>
+                  </table>
+                  <button @click="toggleShopTable()">關閉</button>
+                </div>
+              </template>
+            </tbody>
           </table>
           <div class="button">
-      <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)" :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
-    </div>
-            <!-- 商品資訊最後要放進div做成toggle -->
-    <div v-if="isShopTableVisible" class="registration col-12 col-sm-8 col-md-8">
-      <table class="shop">
-        <caption>訂單明細</caption>
-        <tr>
-          <th>商品編號</th>
-          <th>商品名稱</th>
-          <th>商品數量</th> 
-          <th>單價</th>
-        </tr>
-        <tr v-for="shops in shoplists" :key="shops.PO_ID">
-          <td>{{shops.P_ID}}</td>
-          <td>{{shops.P_NAME}}</td>
-          <td>{{shops.PO_QTY}}</td>
-          <td>{{shops.PO_PRICE}}</td>
-        </tr>
-      </table>
-      <button @click="toggleShopTable()">關閉</button>
-    </div>
+            <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)"
+              :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
+          </div>
         </div>
+
         <!-- 活動查詢 -->
         <div class="registration col-12 col-sm-8  col-md-8" v-else-if="currentSection === 'activity'">
           <table class="apply">
             <caption>活動報名</caption>
             <thead>
-            <tr>
-              <th>活動訂單</th>
-              <th>活動名稱</th>
-              <th>活動日期</th>
-              <th>截止日期</th>
-              <th>地點</th>
-              <th>活動狀態</th>
-              <th>功能</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for=" act in paginatedData" :key="act.E_ID">
-              <td data-label="活動訂單">{{act.E_ID}}</td>
-              <td data-label="活動名稱">{{act.E_TITLE}}</td>
-              <td data-label="活動日期">{{act.E_DATE}}</td>
-              <td data-label="截止日期">{{act.E_DEADLINE}}</td>
-              <td data-label="地點">{{act.E_ADDRESS}}</td>
-              <td data-label="活動狀態">{{act.E_STATUS}}</td>
-              <td data-label="功能"><button>{{act.EO_status}}
-                {{ act.F_ID }}</button></td>
-            </tr>
-          </tbody>
+              <tr>
+                <th>活動訂單</th>
+                <th>活動名稱</th>
+                <th>活動日期</th>
+                <th>截止日期</th>
+                <th>地點</th>
+                <th>活動狀態</th>
+                <th>功能</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for=" act in paginatedData" :key="act.E_ID">
+                <td data-label="活動訂單">{{ act.E_ID }}</td>
+                <td data-label="活動名稱">{{ act.E_TITLE }}</td>
+                <td data-label="活動日期">{{ act.E_DATE }}</td>
+                <td data-label="截止日期">{{ act.E_DEADLINE }}</td>
+                <td data-label="地點">{{ act.E_ADDRESS }}</td>
+                <td data-label="活動狀態">{{ act.E_STATUS }}</td>
+                <td data-label="功能"><button>{{ act.EO_status }}
+                    {{ act.F_ID }}</button></td>
+              </tr>
+            </tbody>
           </table>
           <div class="button">
-      <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)" :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
-    </div>
+            <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)"
+              :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
+          </div>
         </div>
         <!-- 收藏商品 -->
         <div class="registration col-12 col-sm-8  col-md-8" v-else-if="currentSection === 'favorites'">
           <table class="favorite">
             <caption>收藏商品</caption>
             <thead>
-            <tr>
-              <th>商品編號</th>
-              <th>商品名稱</th>
-              <th>商品類別</th>
-              <th>單價</th>
-              <th>功能</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for=" love in paginatedData" :key="love.P_ID">
-              <td data-label="商品編號">{{love.P_ID}}</td>
-              <td data-label="商品名稱">{{love.P_NAME}}</td>
-              <td data-label="商品類別">{{love.PS_ID}}</td>
-              <td data-label="單價">{{love.P_PRICE}}</td>
-              <td data-label="功能"><button>{{love.collect}}</button></td>
-            </tr>
-          </tbody>
+              <tr>
+                <th>商品編號</th>
+                <th>商品名稱</th>
+                <th>商品類別</th>
+                <th>單價</th>
+                <th>功能</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for=" love in paginatedData" :key="love.P_ID">
+                <td data-label="商品編號">{{ love.P_ID }}</td>
+                <td data-label="商品名稱">{{ love.P_NAME }}</td>
+                <td data-label="商品類別">{{ love.PS_ID }}</td>
+                <td data-label="單價">{{ love.P_PRICE }}</td>
+                <td data-label="功能"><button>{{ love.collect }}</button></td>
+              </tr>
+            </tbody>
           </table>
           <div class="button">
-      <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)" :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
-    </div>
+            <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)"
+              :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
+          </div>
         </div>
         <!-- 捐款查詢 -->
         <div class="registration col-12 col-sm-8  col-md-8" v-else-if="currentSection === 'donations'">
           <table class="donate">
             <caption>捐款查詢</caption>
             <thead>
-            <tr>
-              <th>捐款訂單</th>
-              <th>捐款日期</th>
-              <th>捐款金額</th>
-              <th>捐款方式</th>
-            </tr>
-          </thead>
-          <tbody>
-      <tr v-for=" donate in paginatedData" :key="donate.DO_ID">
-        <td data-label="捐款訂單">{{donate.DO_ID}}</td>
-        <td data-label="捐款日期">{{donate.DO_DATE}}</td>
-        <td data-label="捐款金額">{{donate.DO_AMOUNT}}</td>
-        <td data-label="捐款方式">{{donate.PM_NAME}}</td>
-      </tr>
-        </tbody>
+              <tr>
+                <th>捐款訂單</th>
+                <th>捐款日期</th>
+                <th>捐款金額</th>
+                <th>捐款方式</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for=" donate in paginatedData" :key="donate.DO_ID">
+                <td data-label="捐款訂單">{{ donate.DO_ID }}</td>
+                <td data-label="捐款日期">{{ donate.DO_DATE }}</td>
+                <td data-label="捐款金額">{{ donate.DO_AMOUNT }}</td>
+                <td data-label="捐款方式">{{ donate.PM_NAME }}</td>
+              </tr>
+            </tbody>
           </table>
           <div class="button">
-            <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)" :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
+            <a href="#" v-for="pageNum in totalPages" :key="pageNum" @click.prevent="goToPage(pageNum)"
+              :class="{ active: pageNum === currentPage }">{{ pageNum }}</a>
           </div>
         </div>
         <div class="profile-revise col-12 col-sm-8  col-md-8" v-else-if="currentSection === 'password'">
@@ -219,7 +225,7 @@
 
 <script>
 import { store, fetchProfile, logout as logoutStore } from '@/store.js';
-  import { ref, computed, onMounted} from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 fetchProfile()
 
 export default {
@@ -229,7 +235,7 @@ export default {
     const fileInput = ref(null);
     const currentSection = ref('profile');
     const selectedOption = ref('')
-  
+
     const donates = ref([]);
     const currentPage = ref(1);
     const perPage = 10;
@@ -237,7 +243,9 @@ export default {
     const favorites = ref([]);
     const order = ref([]);
     const shoplists = ref([]);
+
     const isShopTableVisible = ref(false);
+    const currentPoId = ref('')
 
     const fileChange = (event) => {
       const file = event.target.files[0];
@@ -250,10 +258,10 @@ export default {
       }
     };
     const logout = () => {
-    logoutStore();
+      logoutStore();
     }
 
-    
+
     const changeSection = (section) => {
       currentSection.value = section
     }
@@ -264,42 +272,34 @@ export default {
     onMounted(async () => {
       try {
         // 使用 Promise.all 来并行获取多个 JSON 数据
-        const responses  = await Promise.all([
-<<<<<<< HEAD
-        fetch(`${import.meta.env.BASE_URL}json/donatemoney.json`),
-        fetch(`${import.meta.env.BASE_URL}json/activities.json`),
-        fetch(`${import.meta.env.BASE_URL}json/favorite.json`),
-        fetch(`${import.meta.env.BASE_URL}json/orders.json`),
-        fetch(`${import.meta.env.BASE_URL}json/shoplist.json`),
-=======
+        const responses = await Promise.all([
           fetch(`${import.meta.env.BASE_URL}json/donatemoney.json`),
           fetch(`${import.meta.env.BASE_URL}json/activities.json`),
           fetch(`${import.meta.env.BASE_URL}json/favorite.json`),
           fetch(`${import.meta.env.BASE_URL}json/orders.json`),
           fetch(`${import.meta.env.BASE_URL}json/shoplist.json`),
->>>>>>> 86ccb2ca8636a9739fa0fbc39a83b2d69bdec433
 
         ]);
-        // 检查每个响应的状态
+        // 檢查每個響應的狀態
         const jsonDatas = await Promise.all(responses.map(async (response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        }));
+
+        // 將每個 JSON 資料整合到一個數組中，或根據需要進行其他處理
+        donates.value = jsonDatas[0];
+        activities.value = jsonDatas[1];
+        favorites.value = jsonDatas[2];
+        order.value = jsonDatas[3];
+        shoplists.value = jsonDatas[4];
+      } catch (error) {
+        console.error('Failed to fetch donation data:', error);
       }
-      return response.json();
-    }));
+    });
 
-    // 将每个 JSON 数据整合到一个数组中，或者根据需要进行其他处理
-    donates.value = jsonDatas[0];
-    activities.value = jsonDatas[1];
-    favorites.value = jsonDatas[2];
-    order.value = jsonDatas[3];
-    shoplists.value = jsonDatas[4];
-    } catch (error) {
-    console.error('Failed to fetch donation data:', error);
-    }
-      });
-
-      const totalPages = computed(() => {
+    const totalPages = computed(() => {
       let dataLength = 0;
       if (currentSection.value === 'donations') {
         dataLength = donates.value.length;
@@ -334,20 +334,35 @@ export default {
         currentPage.value = page;
       }
     };
+    
+
     //顯示清單明細 
     const filteredShoplists = computed(() => {
-      if (!order.value) return [];
-      return shoplists.value.filter(shop => shop.PO_ID === order.value.PO_ID);
+      if (!currentPoId.value) return [];
+      return shoplists.value.filter(shop => shop.PO_ID === currentPoId.value);
     });
 
+
     const toggleShopTable = (poId) => {
-      if (order.value.PO_ID === poId) {
-        isShopTableVisible.value = !isShopTableVisible.value;
+      if (!poId) {
+        isShopTableVisible.value = false
+        return
+      }
+
+      if (currentPoId.value === `${poId}`) {
+        isShopTableVisible.value = false
       } else {
-        order.value.PO_ID = poId;
+        currentPoId.value = `${poId}`;
         isShopTableVisible.value = true;
       }
     };
+
+    watch(isShopTableVisible, (isOpen) => {
+      if (!isOpen) {
+        currentPoId.value = ''
+      }
+    })
+
     return {
       fileInput,
       imageSrc,
@@ -369,6 +384,7 @@ export default {
       paginatedData,
       goToPage,
       isShopTableVisible,
+      currentPoId,
       toggleShopTable,
       filteredShoplists,
     }
