8245d4487c63cf087abaa3fd1aad41b7445fef7d
justin <justin20745413@gmail.com>
2024-07-01 06:59:53 +0800

    add ask dongdong

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,40 +1,109 @@
 <template>
   <div class="text123">
-    <h1>Knowledge Data from PHP API</h1>
+    <h1>Knowledge List</h1>
+    <input v-model="newItem.K_TITLE" placeholder="名稱" />
+    <input v-model="newItem.K_CONTENT" placeholder="內容" />
+    <input v-model="newItem.K_FROM" placeholder="來源" />
+    <input v-model="newItem.K_URL" placeholder="URL" />
+    <input v-model="newItem.K_DATE" type="date" placeholder="日期" />
+    <button @click="addItem">新增</button>
+
     <ul>
-      <li v-for="item in knowledgeData" :key="item.id">{{ item.E_TITLE }}: {{ item.E_ADDRESS }}</li>
+      <li v-for="item in knowledge" :key="item.id">
+        {{ item.K_TITLE }} - {{ item.K_CONTENT }} - {{ item.K_FROM }} - {{ item.K_URL }} - {{ item.K_DATE }}
+        <button @click="deleteItem(item.id)">删除</button>
+      </li>
     </ul>
+
+    <div v-if="error">{{ errorMsg }}</div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 
-
 export default {
   data() {
     return {
-      knowledgeData: []
+      knowledge: [],
+      newItem: {
+        K_TITLE: '',
+        K_CONTENT: '',
+        K_FROM: '',
+        K_URL: '',
+        K_DATE: ''
+      },
+      error: false,
+      errorMsg: '',
     };
   },
   mounted() {
-    this.fetchKnowledgeData();
+    this.fetchData();
   },
   methods: {
-    async fetchKnowledgeData() {
+    async fetchData() {
       try {
-        const response = await axios.get('http://localhost/api/knowledge.php');
-        this.knowledgeData = response.data;
+        const response = await axios.get('http://localhost/cid101/g1/php/knowledge.php');
+        if (!response.data.error) {
+          this.knowledge = response.data.knowledge;
+        } else {
+          this.error = true;
+          this.errorMsg = response.data.msg;
+        }
       } catch (error) {
-        console.error(error);
+        this.error = true;
+        this.errorMsg = error.message;
+      }
+    },
+    async addItem() {
+      try {
+        const response = await axios.post('http://localhost/cid101/g1/php/knowledgeAdd.php', this.newItem, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        if (!response.data.error) {
+          this.fetchData();
+          this.newItem = {
+            K_TITLE: '',
+            K_CONTENT: '',
+            K_FROM: '',
+            K_URL: '',
+            K_DATE: ''
+          };
+        } else {
+          this.error = true;
+          this.errorMsg = response.data.msg;
+        }
+      } catch (error) {
+        this.error = true;
+        this.errorMsg = error.message;
+      }
+    },
+    async deleteItem(id) {
+      try {
+        const response = await axios.post('http://localhost/cid101/g1/php/knowledgeDelete.php', { id }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        if (!response.data.error) {
+          this.fetchData();
+        } else {
+          this.error = true;
+          this.errorMsg = response.data.msg;
+        }
+      } catch (error) {
+        this.error = true;
+        this.errorMsg = error.message;
       }
     }
-  }
+  },
 };
 </script>
 
-<style>
+<style scoped>
 .text123 {
-  margin-top: 100px
+  margin-top: 100px;
 }
 </style>
