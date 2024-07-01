f6cc8ae3a383d39e6390b32f18678f8207c70b90
creat1010 <play56782@gmail.com>
2024-07-01 12:12:33 +0800

    nothing

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -5,7 +5,7 @@
       <li v-for="item in knowledgeData" :key="item.id">{{ item.E_TITLE }}: {{ item.E_ADDRESS }}</li>
     </ul>
   </div>
-  <input type="text" name="box" id="" v-model="again">
+  <input type="text"  id="" v-model="again">
   <button>新增</button>
 </template>
 
