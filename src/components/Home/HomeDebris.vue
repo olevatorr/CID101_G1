9bce7025b89042ac3747d66987a9301bc14180b6
olevatorr <otis.coding@gmail.com>
2024-07-11 00:27:35 +0800

    adjust/ png to webp, events / share/news in index convert json to axios

--- src/components/Home/HomeDebris.vue
+++ src/components/Home/HomeDebris.vue
@@ -41,12 +41,11 @@ const totalSessions = ref(null)
 
 onMounted(async () => {
     try {
-        const response = await axios.post(`${import.meta.env.VITE_API_URL}/DebrisData.php`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/DebrisData.php`);
         if (response.data) {
             console.log(response.data.DEBRIS_DATA);
             hebrisData.value = response.data.DEBRIS_DATA;
-            // console.log(hebrisData.value);
-            console.log(new Date(hebrisData.value[0].DDL_DATE));
+            console.log(hebrisData.value);
         }
     } catch (error) {
         console.error(error);
@@ -60,9 +59,10 @@ const filteredData = computed(() => {
     return {}
 })
 watch(filteredData, () => {
+    console.log(filteredData.value);
     const weightValue = filteredData.value.DD_BEACH_CLEANING || '0';
-    const participantsValue = removeCommas(filteredData.value.DD_ATTENDANCE_TOTAL || '0');
-    const sessionsValue = removeCommas(filteredData.value.DD_CLEANING_TIMES || '0');
+    const participantsValue = filteredData.value.DD_ATTENDANCE_TOTAL || '0';
+    const sessionsValue = filteredData.value.DD_CLEANING_TIMES || '0';
 
     animateNumber(totalWeight.value, weightValue);
     animateNumber(totalParticipants.value, participantsValue);
@@ -79,10 +79,6 @@ function animateNumber(element, targetValue) {
             element.innerHTML = formatNumber(element.textContent);
         },
     });
-}
-
-function removeCommas(value) {
-    return value.replace(/,/g, '');
 }
 
 function formatNumber(value) {
--- src/components/Home/HomeDonation.vue
+++ src/components/Home/HomeDonation.vue
@@ -1,161 +1,170 @@
 <script setup>
-import { RouterLink } from 'vue-router';
-import { Chart } from 'chart.js/auto';
-import { ref, onMounted, } from 'vue';
+import { RouterLink } from 'vue-router'
+import { Chart } from 'chart.js/auto'
+import { ref, onMounted } from 'vue'
+import axios from 'axios';
 
-const donateList = [
-    {
-        "id": 1,
-        "sort": "海洋生態保育專案",
-        "title": "珊瑚礁復育計畫資助",
-        "amount": 300000,
-        "date": "2023/7/1"
-    },
-    {
-        "id": 2,
-        "sort": "相關研究計畫",
-        "title": "海洋塑膠污染研究項目",
-        "amount": 200000,
-        "date": "2023/8/15"
-    },
-    {
-        "id": 3,
-        "sort": "淨灘活動",
-        "title": "全國淨灘日活動經費",
-        "amount": 180000,
-        "date": "2023/9/20"
-    },
-    {
-        "id": 4,
-        "sort": "教育宣導活動",
-        "title": "校園海洋環保講座系列",
-        "amount": 150000,
-        "date": "2023/10/5"
-    },
-    {
-        "id": 5,
-        "sort": "行政及人事開支",
-        "title": "海洋保護組織運營費用",
-        "amount": 150000,
-        "date": "2023/11/10"
-    },
-    {
-        "id": 6,
-        "sort": "網站維運及更新",
-        "title": "海洋環保資料庫建設",
-        "amount": 20000,
-        "date": "2023/12/1"
+const error = ref('')
+const errorMsg = ref('')
+const donateList = ref(null)
+const expenditure = ref(null)
+const expenditureAmount = ref(null)
+const expenditureList = ref(null)
+const donateAmount = ref('')
+
+const fetchDonateData = async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/Donate.php`)
+    if (!response.data.error) {
+        donateAmount.value = response.data.data.reduce((acc, cur) => acc+=cur.DO_AMOUNT,0)
+        donateList.value = response.data.data.sort((a, b) => b.DO_ID - a.DO_ID).slice(0,6)
+    } else {
+      error.value = true
+      errorMsg.value = response.data.msg
     }
-]
+  } catch (error) {
+    console.error(error) // 打印错误信息
+    error.value = true
+    errorMsg.value = error.message
+  }
+}
+
+const fetchExpenditureData = async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/Expenditure.php`)
+    if (!response.data.error) {
+        expenditureAmount.value = response.data.reduce((acc, cur) => acc+=cur.EL_OUTLAY,0)
+      expenditure.value = response.data.sort((a, b) => b.EL_ID - a.EL_ID).slice(0,6)
+      expenditureList.value = response.data
+      console.log('expenditure:' ,expenditureList.value);
+    } else {
+      error.value = true
+      errorMsg.value = response.data.msg
+    }
+  } catch (error) {
+    console.error(error) // 打印错误信息
+    error.value = true
+    errorMsg.value = error.message
+  }
+}
+
+const converDate = (date) => {
+    return date.slice(0,10).replace(/-/g, '/')
+}
+
+
+
 
 const donateDistribution = [
-    { id: 1, sort: '海洋生態保育專案', data: 30, color: '#6CE5E8' },
-    { id: 2, sort: '相關研究計畫', data: 20, color: '#41B8D5' },
-    { id: 3, sort: '淨攤活動', data: 18, color: '#2D8BBA' },
-    { id: 4, sort: '教育宣導活動', data: 15, color: '#2F5F98' },
-    { id: 5, sort: '行政及人事開支', data: 15, color: '#31356E' },
-    { id: 6, sort: '網站維護', data: 5, color: '5E17EB' },
-];
+  { id: 1, sort: '海洋生態保育專案', data: 30, color: '#6CE5E8' },
+  { id: 2, sort: '相關研究計畫', data: 20, color: '#41B8D5' },
+  { id: 3, sort: '淨攤活動', data: 18, color: '#2D8BBA' },
+  { id: 4, sort: '教育宣導活動', data: 15, color: '#2F5F98' },
+  { id: 5, sort: '行政及人事開支', data: 15, color: '#31356E' },
+  { id: 6, sort: '網站維護', data: 5, color: '5E17EB' }
+]
 
-const donateChart = ref(null);
+const donateChart = ref(null)
 
-onMounted(() => {
-    const dataNum = donateDistribution.map(item => item.data);
-    const labels = donateDistribution.map(item => item.sort + ' ' + item.data + '%');
-    const colors = donateDistribution.map(item => item.color);
+onMounted(async () => {
+    await fetchDonateData()
+    await fetchExpenditureData()
+  const dataNum = donateDistribution.map((item) => item.data)
+  const labels = donateDistribution.map((item) => item.sort + ' ' + item.data + '%')
+  const colors = donateDistribution.map((item) => item.color)
 
-    const ctx = donateChart.value.getContext('2d');
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: dataNum,
-                backgroundColor: colors,
-                borderColor: 'rgba(0,0,0,0.1)'
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-            plugins: {
-                legend: {
-                    position: 'top',
-                    labels: {
-                        usePointStyle: true,
-                        color: '#000'
-                    },
-                    padding: 20
-                },
-            },
-            layout: {
-                padding: 0
-            }
+  const ctx = donateChart.value.getContext('2d')
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          data: dataNum,
+          backgroundColor: colors,
+          borderColor: 'rgba(0,0,0,0.1)'
         }
-    });
-});
+      ]
+    },
+    options: {
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          position: 'top',
+          labels: {
+            usePointStyle: true,
+            color: '#000'
+          },
+          padding: 20
+        }
+      },
+      layout: {
+        padding: 0
+      }
+    }
+  })
+})
 </script>
 
 <template>
-    <section class="section section-donation-summary">
-        <div class="container">
-            <h3>
-                DONATION<br>
-                捐款總攬
-            </h3>
-            <div class="row">
-                <div class="col-12 col-lg-6 donate-num">
-                    <div class="clean-tons">
-                        <span class="debris-word">總金額</span>
-                        <span class="debris-num">9,309,090</span>
-                        <span class="debris-word">元</span>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-6 donate-num">
-                    <div class="clean-tons donate-balance">
-                        <span class="debris-word">剩餘</span>
-                        <span class="debris-num">920,090</span>
-                        <span class="debris-word">元</span>
-                    </div>
-                </div>
-                <div class="col-12 col-lg-6">
-                    <canvas ref="donateChart" width="100%" height="500"></canvas>
-                </div>
-                <div class="col-12 col-lg-6">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th class="donate-col">類別</th>
-                                <th class="donate-col">名目</th>
-                                <th class="donate-col">金額</th>
-                                <th class="donate-col">日期</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="item in donateList" :key="item.id">
-                                <th>{{ item.sort }}</th>
-                                <td>{{ item.title }}</td>
-                                <td>{{ item.amount }}</td>
-                                <td>{{ item.date }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <p>*最新的六筆支出</p>
-                </div>
-            </div>
-            <div class="col-12 donate-link">
-                <div class="hover-zone">
-                    <router-link to="/donate">
-                        <div class="donate-btn">
-                            <div class="donate-btn-circle">
-                                <i class="fa-solid fa-dollar-sign"></i>
-                            </div>
-                            <div class="donate-txt">
-                                立即捐款
-                            </div>
-                        </div>
-                    </router-link>
-                </div>
-            </div>
+  <section class="section section-donation-summary">
+    <div class="container">
+      <h3>
+        DONATION<br />
+        捐款總攬
+      </h3>
+      <div class="row">
+        <div class="col-12 col-lg-6 donate-num">
+          <div class="clean-tons">
+            <span class="debris-word">總金額</span>
+            <span class="debris-num">9,309,090</span>
+            <span class="debris-word">元</span>
+          </div>
         </div>
-    </section>
+        <div class="col-12 col-lg-6 donate-num">
+          <div class="clean-tons donate-balance">
+            <span class="debris-word">剩餘</span>
+            <span class="debris-num">920,090</span>
+            <span class="debris-word">元</span>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6">
+          <canvas ref="donateChart" width="100%" height="500"></canvas>
+        </div>
+        <div class="col-12 col-lg-6">
+          <table>
+            <thead>
+              <tr>
+                <th class="donate-col">類別</th>
+                <th class="donate-col">名目</th>
+                <th class="donate-col">金額</th>
+                <th class="donate-col">日期</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in expenditure" :key="item.id">
+                <th>{{ item.EL_NAME }}</th>
+                <td>{{ item.EL_TITLE }}</td>
+                <td>{{ item.EL_OUTLAY }}</td>
+                <td>{{ converDate(item.EL_DATE) }}</td>
+              </tr>
+            </tbody>
+          </table>
+          <p>*最新的六筆支出</p>
+        </div>
+      </div>
+      <div class="col-12 donate-link">
+        <div class="hover-zone">
+          <router-link to="/donate">
+            <div class="donate-btn">
+              <div class="donate-btn-circle">
+                <i class="fa-solid fa-dollar-sign"></i>
+              </div>
+              <div class="donate-txt">立即捐款</div>
+            </div>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
